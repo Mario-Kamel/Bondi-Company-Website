b23ebe76cded6ab341492f22368fccd866e7842d
Mario Kamel <mario.medhat99@gmail.com>
2024-03-07 11:10:11 +0200

    Make form compatible with netlify

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,20 +2,36 @@ import React, { useState } from "react";
 import strings from "../Assets/localization";
 
 const Contact = () => {
-  const [email, setEmail] = useState("yourmail@domain");
+  const [email, setEmail] = useState(""); // Initialize email state
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value); // Update email state when input changes
+  };
 
   const submitForm = (event) => {
     event.preventDefault();
     alert("We will contact you soon!");
-    setEmail("");
-  }
+    setEmail(""); // Clear email input after submission
+  };
+
   return (
     <div className="contact-page-wrapper">
       <h1 className="primary-heading">{strings.contactQuestion}</h1>
       <h1 className="primary-heading">{strings.letUsHelp}</h1>
       <div className="contact-form-container">
-        <input type="text" placeholder={email} />
-        <button className="secondary-button" onClick={submitForm}>{strings.submit}</button>
+        <form name="contact" onSubmit={submitForm} netlify>
+          {/* Input field for email */}
+          <input
+            type="email"
+            placeholder="yourmail@domain"
+            value={email} // Bind input value to email state
+            onChange={handleEmailChange} // Handle input changes
+          />
+          {/* Submit button */}
+          <button className="secondary-button" type="submit">
+            {strings.submit}
+          </button>
+        </form>
       </div>
     </div>
   );
